f1c11dc9c827d4630602cbf7ed53ea5a4876e8dd
Matt Daniell <sube1even@users.noreply.github.com>
2022-12-06 18:27:58 +1030

    Update index.js

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ function formatCSS (css) {
 }
 
 function formatHTML (html) {
-  html = prettier.format(html, { parser: 'html', printWidth: 200, embeddedLanguageFormatting: 'off' })
+  html = prettier.format(html, { parser: 'html', printWidth: 200, embeddedLanguageFormatting: 'off', htmlWhitespaceSensitivity: 'ignore' })
 
   // Cut the timestamp line
   const start = html.indexOf('\n') + 1
